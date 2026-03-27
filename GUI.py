2e3fe8ea4c9c335f7733250216787dc194b69a1d
Ravina <hema130384@gmail.com>
2026-03-27 13:35:43 +0530

    adding text box

--- GUI.py
+++ GUI.py
@@ -22,6 +22,12 @@ image = ImageTk.PhotoImage(Image.open("image/image.png"))
 image_Label = Label(frame , image = image)
 image_Label.grid(row = 1, column = 0, pady= 20)
 
+# adding a text widget
+text = Text(root , font = ('courier 10 bold'), bg = "beige")
+text.grid(row = 2, column = 0, )
+text.place(x= 60, y = 300, width = 400, height= 100)
+
+# entry widget
 
 
 
