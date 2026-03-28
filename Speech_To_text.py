180ac9c550be3f754bf6c0a19474d4c60aa7e9c2
Ravina <hema130384@gmail.com>
2026-03-28 10:02:21 +0530

    speech to text code done

--- Speech_To_text.py
+++ Speech_To_text.py
@@ -1,15 +1,21 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Speak something...")
-    audio = r.listen(source)
+def speech_to_txt():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+     print("Speak something...")
+     audio = r.listen(source)
 
     try:
-        text = r.recognize_google(audio)
-        print("You said:", text)
+        voice_data = ""
+        voice_data = r.recognize_google(audio)
+        print("You Said: ",voice_data)
+        return voice_data 
     except sr.UnknownValueError:
         print("Sorry, could not understand audio")
     except sr.RequestError:
         print("API error / No internet") 
+
+speech_to_txt()
