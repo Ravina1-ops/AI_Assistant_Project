19f81c0e3d89851a48f899b4dc972b73782bd02a
Ravina <hema130384@gmail.com>
2026-03-28 09:48:06 +0530

    speech to text code done using speech recognition and pyaudio library

--- Speech_To_text.py
+++ Speech_To_text.py
@@ -1,24 +1,15 @@
-import sounddevice as sd
 import speech_recognition as sr
-import numpy as np
 
 r = sr.Recognizer()
 
-# Record 5 seconds of audio from mic
-duration = 5  # seconds
-print("Recording...")
-audio_data = sd.rec(int(duration * 16000), samplerate=16000, channels=1, dtype='int16')
-sd.wait()
-print("Recording complete.")
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
 
-# Convert to AudioData for SpeechRecognition
-audio_bytes = audio_data.tobytes()
-audio = sr.AudioData(audio_bytes, 16000, 2)
-
-# Recognize speech
-try:
-    print("You said: " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Sorry, I could not understand.")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+    try:
+        text = r.recognize_google(audio)
+        print("You said:", text)
+    except sr.UnknownValueError:
+        print("Sorry, could not understand audio")
+    except sr.RequestError:
+        print("API error / No internet") 
